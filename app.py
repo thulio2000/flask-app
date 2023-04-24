e02d1c8034902ab0f59c84f35fbdfdf1c6084943
Túlio <lucastuliolima@gmail.com>
2023-04-24 19:20:59 -0300

    "Added home.html"

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return '<h1>Hello, Flask!</h1>'
+def home():
+    return render_template("home.html  ")
+
+
+@app.route("/shire")
+def shire():
+    return '<h1>Hello, Shire!</h1>'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- micro.py
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env python
-from flask import Flask
-app = Flask(__name__)

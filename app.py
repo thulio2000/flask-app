5433d3ee8d55ad7d3e6c51a2c1b4afd4ceda790f
Túlio <lucastuliolima@gmail.com>
2023-04-25 17:11:23 -0300

    "About.html and parent template for navigation"

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html  ")
+    return render_template("home.html")
 
 
-@app.route("/shire")
-def shire():
-    return '<h1>Hello, Shire!</h1>'
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":

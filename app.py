73cd73c4922681bb8f97f060e2ac8eabe0ab4662
Túlio <lucastuliolima@gmail.com>
2023-05-03 08:38:41 -0300

    "Commenting app.py"

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__) #creates the Flask application instance, __name__ tells flask to use current Python module as the app name
 
 
-@app.route("/")
-def home():
+@app.route("/") #defining the route for the root path/homepage
+def home(): #executed when the user navigates to the path defined above, rendering the home.html
     return render_template("home.html")
 
 
-@app.route("/about")
-def about():
+@app.route("/about") #defines about route
+def about(): #executed when the user navigates to /about, rendering the about.html
     return render_template("about.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__": #
+    app.run(debug=True) #starts the app in debug mode
